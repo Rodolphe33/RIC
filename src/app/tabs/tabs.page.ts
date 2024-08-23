0bc6bc34998a8ed979acf33e4a2227f7569ff735
Rodolphe_Lassalle <rlassalle@epsyl-alcen.com>
2024-08-23 16:48:17 +0200

    rename prefixe

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,10 +1,10 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { triangle, ellipse, square } from 'ionicons/icons';
+import { archive, statsChart, accessibility } from 'ionicons/icons';
 
 @Component({
-  selector: 'app-tabs',
+  selector: 'ric-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
   standalone: true,
@@ -14,6 +14,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square });
+    addIcons({ archive, statsChart, accessibility });
   }
 }
