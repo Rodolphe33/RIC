cd1ced4d9f62fd4ba84e388bf35a3acf7d603028
Rodolphe_Lassalle <rlassalle@epsyl-alcen.com>
2024-08-23 16:01:29 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ric',
+  webDir: 'www'
+};
+
+export default config;
